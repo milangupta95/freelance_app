4cc39373d216a1761ff8aca67f50397ff202913f
milangupta95 <milangupta95@gmail.com>
2024-07-06 22:44:58 +0530

    Added

--- src/Components/Cards/TechPartCard.jsx
+++ src/Components/Cards/TechPartCard.jsx
@@ -5,7 +5,7 @@ import { LuArrowUpRight } from "react-icons/lu";
 export const TechPartCard = ({ imgsrc, link }) => {
     return (
         <div className='p-4 bg-[#FFAE2F] space-y-3 flex flex-col justify-center items-center bg-opacity-[25%]'>
-            <div className='relative h-[70px] w-[150px] md:h-[100px] md:w-[200px]'>
+            <div className='relative h-[60px] w-[120px] md:h-[100px] md:w-[200px]'>
                 <Image layout='fill' src={imgsrc}></Image>
             </div>
             <div>
