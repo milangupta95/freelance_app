aad0f3a8e8e8b0234895b52b4ea60f029c651ea2
milangupta95 <milangupta95@gmail.com>
2024-07-06 22:52:18 +0530

    TecPart:Improved

--- src/app/technologypartner/page.jsx
+++ src/app/technologypartner/page.jsx
@@ -69,23 +69,25 @@ const Page = () => {
     return (
         <div className="text-black bg-white space-y-4">
             <div
-                className="px-4 md:px-10 lg:px-20 flex items-center h-[50vh] w-full bg-cover bg-center"
+                className="h-[50vh] w-full bg-cover bg-center"
                 style={{ backgroundImage: `url(${"/images/banners/techpartbanner.png"})` }}
             >
-                <div className="flex flex-col space-y-4 md:space-y-8 max-w-full md:max-w-2xl lg:max-w-none">
-                    <div className="border-[#F19F1F] border-l-4 p-4 space-y-2 md:space-y-4 px-2 md:px-4">
-                        <h1 className="text-2xl text-white font-bold md:text-3xl lg:text-4xl">
-                            Technology Partner
-                        </h1>
-                        <p className="w-full" style={{ color: 'white' }}>
-                            Empowering innovation through seamless technology partnerships.
-                        </p>
+                <div className="bg-black px-4 md:px-10 lg:px-20 flex items-center  w-full h-full bg-opacity-10 backdrop-blur-sm">
+                    <div className="flex flex-col space-y-4 md:space-y-8 max-w-full md:max-w-2xl lg:max-w-none">
+                        <div className="border-[#F19F1F] border-l-4 p-4 space-y-2 md:space-y-4 px-2 md:px-4">
+                            <h1 className="text-2xl text-white font-bold md:text-3xl lg:text-4xl">
+                                Technology Partner
+                            </h1>
+                            <p className="w-full" style={{ color: 'white' }}>
+                                Empowering innovation through seamless technology partnerships.
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
             <div className="w-full p-4 grid grid-cols-2 md:grid-cols-5 md:gap-y-8 gap-y-4   justify-items-center">
                 {
-                    partners.map((partner,idx) => {
+                    partners.map((partner, idx) => {
                         return <TechPartCard key={idx} imgsrc={partner.imageSrc} link={partner.link} />
                     })
                 }
